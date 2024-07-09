8fdb07f0d72b33a59cffc2ff6aa88c3e948deb62
Abhishek Kumar <brightlight052023@gmail.com>
2024-07-09 22:04:12 +0530

    my first commit

---
+++ js/a.js
@@ -0,0 +1,13 @@
+
+function redirect(){
+    // document.getElementsByClassName("btn-register")[0].style.backgroundColor="#DFC57B";
+    
+    // window.location.href="https://6abhishek.github.io/invertis/";
+
+    const name = document.querySelector("#txtUserid").value;
+    const password = document.querySelector('#ttxtpassword').value;
+
+    if (name == "BC2023259" && password == "6392ak"){
+        window.location.replace("https://6abhishek.github.io/invertis/");
+    }
+}
